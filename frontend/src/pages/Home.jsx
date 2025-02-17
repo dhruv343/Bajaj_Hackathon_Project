a6e774b338fd78a37f9d06afb2b8746bdced12ea
vansh-08 <sharmavansh0836@gmail.com>
2025-02-17 23:27:16 +0530

    Three counters squats, push up and crunches are added

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,14 @@
 import PushUpCounter from "../components/PushUpCounter";
 import SquatCounter from "../components/SquatCounter";
+import CrunchCounter from "../components/CrunchCounter";
 
 function Home() {
     return (
     <>
     <h1>Home Page</h1>
-    {/* <SquatCounter/> */}
+    <SquatCounter/>
+    <PushUpCounter />
+    <CrunchCounter />
     </>
     
     )
